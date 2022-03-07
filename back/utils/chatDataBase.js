50f6a21d27e2db482970a7f2fc53802d35f64511
Rodrigo López <rodrigohernanlopez89@gmail.com>
2022-03-07 13:00:10 -0300

    n

--- back/utils/chatDataBase.js
+++ back/utils/chatDataBase.js
@@ -2,8 +2,6 @@ const userDb = [];
 const chatRooms = ["Rodri", "Erotica"];
 
 function chatRoomStats() {
-  console.log(userDb, "CHATROOMSTATS INICIO");
-
   const stats = chatRooms.reduce((final, chatRoom) => {
     final[chatRoom] = 0;
     userDb.forEach((u) => {
@@ -13,7 +11,7 @@ function chatRoomStats() {
     });
     return final;
   }, {});
-  console.log(stats, "CHATROOMSTATS FIN");
+
   return stats;
 }
 function saveNewUser(user) {
@@ -21,17 +19,16 @@ function saveNewUser(user) {
 }
 //hubo un crash de un user q no estaba en la db cuando recien ingresó, será xq envie muy rapido el msje?
 function findUser({ id }) {
-  console.log(id, "adentro de find users", userDb);
   return userDb.find((user) => user.id === id);
 }
 function removeFromChat(socket) {
   const removedUser = userDb.find((i) => i.id === socket.id);
-  console.log(userDb, "REMOVEFROMCHAT INICIO");
+
   const usersUpdated = userDb.filter((user) => user.id !== socket.id);
   userDb.splice(0);
-  console.log(userDb, "REMOVEFROMCHAT INTERMEDIO");
+
   userDb.push(...usersUpdated);
-  console.log(userDb, "REMOVEFROMCHAT FIN");
+
   return removedUser;
 }
 function findAllUsersOfARoom(chatRoom) {
